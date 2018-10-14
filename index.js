ca2f9f6e2b6c1febb7512196a328345af71ef1dd
Aditya Loshali <adityaloshali1@gmail.com>
2018-10-14 16:10:54 +0530

    cookies enabled on the app for oAuth flow

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,13 +11,23 @@ mongoose.Promise = global.Promise;
 mongoose.connect(encodeURI(keys.mongoURI),{
   useNewUrlParser:true
 },(err, client) => {
-  if (err) {
-    console.log(err);
-  }
+  if (err) { console.log(err); }
   console.log('Connected to Mongo');
 });
 
 const app = express();
+
+// enabling cookies in the app
+// max age( validity ) . 30 days in milliseconds
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+// tell passport to use cookie sessions
+app.use(passport.session());
 
 require('./routes/authRoutes')(app)
 

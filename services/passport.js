ef906019498bd27bc56c1c4b18c005e74990131d
Aditya Loshali <adityaloshali1@gmail.com>
2018-10-14 20:00:08 +0530

    proxy addded

--- services/passport.js
+++ services/passport.js
@@ -29,13 +29,14 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-
+// proxy true to allow proxy servers by heroku
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {

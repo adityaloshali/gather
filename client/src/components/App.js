8c818ab66d43f05bb998136b65a7d761557d4437
Aditya Loshali <adityaloshali1@gmail.com>
2018-10-14 23:49:38 +0530

    add and setup materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
     <div>
-      Hi There!
+      <BrowserRouter>
+        <div>
+          <Header />
+           <div className="container">
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route path="/surveys/new" component={SurveyNew} />
+              <Route path="/surveys" component={Dashboard} />
+            </Switch>
+           </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };

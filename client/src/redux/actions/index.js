fd2e50ede95b139c5776ce8cb73215cfbcede917
Aditya Loshali <adityaloshali1@gmail.com>
2018-10-19 13:56:25 +0530

    auth flow front-to-back

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 const fetchUser = () =>
-  (dispatch) => {
-    axios
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res.data }));
+  async (dispatch) => {
+    const res = await axios.get('/api/current_user');
+    
+    dispatch({ type: FETCH_USER, payload: res.data });
   };
 
 export default {
